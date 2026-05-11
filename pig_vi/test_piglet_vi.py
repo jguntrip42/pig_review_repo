3a301d2bde88851b4a54b14df8280deeb5428b2f
Joseph Guntrip <j.guntrip42@gmail.com>
2026-05-11 16:00:32 +0100

    Modified piglet scripts and added dedicated scripts for plotting and testing

---
+++ pig_vi/test_piglet_vi.py
@@ -0,0 +1,65 @@
+
+from pig_vi import *
+
+piglet_states = [(0,0,0),(0,0,1),(0,1,0),(0,1,1),(1,0,0),(1,1,0),(1,0,1),(1,1,1),(0,0,2),(0,1,2),"WIN"]
+piglet_actions = ["H","F"]
+
+target = 2
+
+
+def P_piglet(s_new,s,a):
+    """Returns probabilities for taking action a in state s and transitioning to state s_new"""
+    
+    if s != "WIN":
+        if a == "F":
+            if (s[0] == s_new[0] and s[1] == s_new[1] and s_new[2] == 0): # Case where we obtain tails
+                p = 0.5
+            elif (s[0] == s_new[0] and s[1] == s_new[1] and (s[2]+1) == s_new[2]): # Case where we obtain heads
+                p = 0.5
+            else:
+                p = 0
+        elif a == "H":
+            if ((s[0]+s[2]) == s_new[0]) and (s[1] == s_new[1]) and (s_new[2] == 0) and ((s[2]+s[0]) != target): # Case where one holds but doesn't win immediately
+                p = 1
+            elif ((s[2]+s[0]) == target) and (s_new == "WIN"): # Case where holding will inveitably cause the player to win
+                p = 1
+            else:
+                p = 0
+    else:
+        p = 0
+        
+    return p
+
+
+def R_piglet(s_new,s,a):
+    """Returns the reward for performing action a and moving from state s to state s_new"""
+    
+    if ((s_new == "WIN") and (a == "H") and ((s[2]+s[0]) == target)): # Reward only when player wins
+        r = 1
+    else:
+        r = 0
+    
+    return r
+
+
+
+
+
+def test_1():
+    
+    new_policy,val_map_0,val_dict_0 = piglet_val_iteration(piglet_states,piglet_actions,P_piglet,R_piglet,epsilon=0.00001)
+    
+    correct_val_map = {(0, 0, 0): 0.5714285969734192, (0, 0, 1): 0.7142857313156128, 
+                       (0, 1, 0): 0.4000133275985718, (0, 1, 1): 0.6000075936317444, 
+                       (1, 0, 0): 0.7999899387359619, (1, 1, 0): 0.6666666865348816, 
+                       (1, 0, 1): 1.0, (1, 1, 1): 1.0, (0, 0, 2): 1.0, (0, 1, 2): 1.0, 'WIN': 0}
+    correct_policy = {(0, 0, 0): 'F', (0, 0, 1): 'F', (0, 1, 0): 'F', (0, 1, 1): 'F', 
+                      (1, 0, 0): 'F', (1, 1, 0): 'F', (1, 0, 1): 'H', (1, 1, 1): 'H', 
+                      (0, 0, 2): 'H', (0, 1, 2): 'H', 'WIN': None}
+
+    
+    assert ((new_policy == correct_policy) and (val_map_0 == correct_val_map))
+
+
+
+
