38eaecb69f04cd416bfabaeba87ed0a3dcb175cb
Joseph Guntrip <j.guntrip42@gmail.com>
2026-05-15 15:07:11 +0100

    Modified file structure

--- pig_vi/run_pig.py
+++ pig_vi/run_pig.py
@@ -1,10 +1,9 @@
-from pig_vi.pig import Pig
+from pig import Pig
 import pickle 
-
-import os.path
 
 game = Pig(target=100)
 game.value_iteration(tolerance=1e-6, max_iterations=10000)
+
 
 print("Pig target:", game.target)
 print("Value at (0, 0, 0):", game.get_value(0, 0, 0))
@@ -12,7 +11,7 @@ print("Policy at (0, 0, 0):", game.get_policy(0, 0, 0))
 
 
 results = {"target": game.target,"states": game.states,"values": game.values, "policy": game.policy}
-with open(os.path.dirname(__file__) + "/pig_vi/pig_vi_results.pkl", "wb") as f:
+with open("pig_vi_results.pkl", "wb") as f:
     pickle.dump(results, f)
 print("Results saved.")
 
